a6ed0f0450532fa271e36d5449055b41d8b27d96
hobbyluv <princewillchime43@gmail.com>
2020-07-11 08:43:02 +0100

    adjust the sign in validation

--- controller/signIn_user.js
+++ controller/signIn_user.js
@@ -1,28 +1,31 @@
 const bcrypt = require("bcryptjs");
 const SignUpUser = require("../models/signUp_users");
+const jwt = require("jsonwebtoken");
 exports.get_user = async (req, res) => {
   const { email: Email, password: pass } = req.body;
   let secureId = await SignUpUser.find({ email: Email });
-  console.log("secureId", secureId);
   if (secureId.length < 1) {
-    console.log("email failed");
     res.status(200).send({ statusr: 401 });
   } else {
     let auth = null;
-    secureId.find(({ email, password, fullName }) => {
-      bcrypt.compare(pass, password, (err, isMatch) => {
-        if (err) {
-          res.status(500).send("server error please try again later");
-        } else if (!isMatch) {
-          res.status(401).send();
-          console.log("not match");
-        } else {
-          auth = { email, fullName };
-          res.status(200).send(auth);
-        }
+    secureId.find(({ email, password, fullName, _id }) => {
+      -bcrypt.compare(pass, password, (err, isMatch) => {
+        bcrypt.compare(pass, password, (err, isMatch) => {
+          if (err) {
+            res
+              .status(200)
+              .send({ error: "server error please try again later" });
+          } else if (!isMatch) {
+            res.status(200).send({ error: 401 });
+          } else {
+            const token = jwt.sign({ userId: _id }, "RAMDOM_TOKEN_SECRET", {
+              expiresIn: "24h"
+            });
+            auth = { fullName, token };
+            res.status(200).send(auth);
+          }
+        });
       });
-      console.log("email", email);
-      console.log("pass", pass);
     });
   }
 };
