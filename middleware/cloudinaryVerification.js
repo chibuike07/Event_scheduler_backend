0c2d971e681b0f13293c7750343b0dcc76b35fbe
hobbyluv <princewillchime43@gmail.com>
2021-06-23 01:29:50 +0100

    installed normalize-url and updated the packages

--- middleware/cloudinaryVerification.js
+++ middleware/cloudinaryVerification.js
@@ -1,40 +1,31 @@
 const multer = require("multer");
-const URL = require("url");
 
-require("dotenv").config();
 const cloudinary = require("cloudinary").v2;
+
 const { CloudinaryStorage } = require("multer-storage-cloudinary");
 
-const cloudinary_url = URL.parse(`${process.env.CLOUDINARY_URL}`);
+const {
+  CLOUDINARY_CLOUD_NAME,
+  CLOUDINARY_API_KEY,
+  CLOUDINARY_API_SECRET,
+} = process.env;
 
 cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
-  //   cloud_name: cloudinary_url.host,
-  //   api_key: cloudinary_url.auth.split(":")[0],
-  //   api_secret: cloudinary_url.auth.split(":")[1],
+  cloud_name: CLOUDINARY_CLOUD_NAME,
+  api_key: CLOUDINARY_API_KEY,
+  api_secret: CLOUDINARY_API_SECRET,
 });
-let parser;
-try {
-  const storage = new CloudinaryStorage({
-    cloudinary: cloudinary,
-    params: {
-      folder: "Admin_event_photo",
-      format: ["png", "jpeg", "jpg"],
-      public_id: (req, file) => file.destination,
-      transformation: [{ width: 500, height: 500, crop: "limit" }],
-    },
-  });
-  console.log("ends here");
 
-  parser = multer({ storage: storage });
-} catch (err) {
-  if (err.response === undefined) {
-    return console.log("got here");
-  } else {
-    console.log(err);
-  }
-}
+const storage = new CloudinaryStorage({
+  cloudinary: cloudinary,
+  params: {
+    folder: "Admin_event_photo",
+    format: async () => "png",
+    public_id: (req, file) => file.destination,
+    allowed_formats: ["png", "jpg", "jpeg"],
+  },
+});
+
+const parser = multer({ storage: storage });
 
 module.exports = { parser };
